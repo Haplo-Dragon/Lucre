6b41320529b5ff30e97b933721a6b0a1d5e4eede
Haplo <haplodragon@gmail.com>
2017-03-02 15:31:48 -0600

    Rearrange regex to make more sense and work faster. Changed transaction loop to be more intuitive. Present beginning exchange rate input in order.

--- lucre.py
+++ lucre.py
@@ -10,63 +10,61 @@ exchange = {'Gold': 1, 'Silver': 10, 'Copper': 100}
 for item in COINS:
     wealth[item] = int(input("How much %s do you have?\n" % item))
 
-for coin_type in wealth:
+for coin_type in sorted(wealth.keys()):
     if coin_type is not 'Gold':
         exchange[coin_type] = int(input("How many %s pieces in 1 gold piece?\n" % coin_type))
 
 
-def transact(starting_wealth, amount, exchange_rate, type):
+def transact(starting_wealth, amount, exchange_rate, transaction_type):
     # Form a list of tuples by pairing coin types and regex letters one to one - ('Gold', '[g]'), ('Silver', '[s]') etc.
-    denominations = zip(COINS, ['[g]', '[s]', '[c]'])
-    for denom in denominations:
-        p = re.compile(r"""
-                        ([0-9]+)        # Any number of digits; the parentheses mean we are capturing this bit
-                                        # for later use.
-                        %s              # Followed by the current denomination (g, s, or c) from the loop
-                        \b              # and ending with a word boundary.
-                        """
-                       % denom[1],  # Here's where we insert the current denomination from the loop
-                       re.VERBOSE)
+    denominations = zip(COINS, ['g', 's', 'c'])
+    denom = list(denominations)
+    p = re.compile(r"""
+                           ([0-9]+)        # Any number of digits; the parentheses mean we are capturing this bit
+                                           # for later use.
+                           [gsc]              # Followed by the current denomination (g, s, or c)
+                           """,
+                   re.VERBOSE)
+    iterator = p.finditer(amount)
 
-        match = p.findall(amount)
-        if match:
-            change = int(match[0])
+    for match in iterator:
+        currency = None
+        for item in denom:
+            if match.group()[-1] == item[1]:
+                currency = item[0]
 
-            if type == 'SPEND':
-                change = -change
+        change = int(match.group(1))
 
-            if starting_wealth[denom[0]] + change < 0:
-                if denom[0] == 'Gold':
-                    print("You can't afford it!")
-                else:
-                    tier_above = COINS.index(denom[0]) - 1
-                    orig_wealth = {}
+        if transaction_type == 'SPEND':
+            change = -change
 
-                    for key, value in starting_wealth.items():
-                        orig_wealth[key] = value
-
-                    while abs(change) > starting_wealth[denom[0]]:
-                        if tier_above < 0:
-                            print("You can't afford it!")
-                            print("Setting starting wealth to: ", orig_wealth)
-                            starting_wealth = orig_wealth
-                            return starting_wealth
-                        if starting_wealth[COINS[tier_above]] > 0:
-                            starting_wealth[COINS[tier_above]] -= 1
-                            starting_wealth[denom[0]] += int(exchange_rate[denom[0]] / exchange_rate[COINS[tier_above]])
-                        else:
-                            tier_above -= 1
-
-                    starting_wealth[denom[0]] += change
+        if starting_wealth[currency] + change < 0:
+            if currency == 'Gold':
+                print("You can't afford it!\n")
+                input("Press any key to feel poor.")
             else:
-                starting_wealth[denom[0]] += change
+                tier_above = COINS.index(currency) - 1
+
+                orig_wealth = {}
+                for key, value in starting_wealth.items():
+                    orig_wealth[key] = value
+
+                while abs(change) > starting_wealth[currency]:
+                    if tier_above < 0:
+                        print("You can't afford it!")
+                        starting_wealth = orig_wealth
+                        return starting_wealth
+                    if starting_wealth[COINS[tier_above]] > 0:
+                        starting_wealth[COINS[tier_above]] -= 1
+                        starting_wealth[currency] += int(exchange_rate[currency] / exchange_rate[COINS[tier_above]])
+                    else:
+                        tier_above -= 1
+
+                starting_wealth[currency] += change
+        else:
+            starting_wealth[currency] += change
 
     return starting_wealth
-
-
-def convert(denom, starting_wealth, change, exchange_rate):
-    while change > starting_wealth[denom[0]]:
-        pass
 
 
 if __name__ == "__main__":
@@ -88,10 +86,10 @@ if __name__ == "__main__":
             choice = None
 
         if choice == 1:
-            price = input("How much did you squander this time? (In the format '4g 10s 56c')\n")
-            wealth = transact(wealth, price, exchange, type='SPEND')
+            price = input("How much did you squander THIS time? (In the format '4g 10s 56c')\n")
+            wealth = transact(wealth, price, exchange, transaction_type='SPEND')
         if choice == 2:
             new_money = input("How much filthy lucre? (In the format '4g 10s 56c')\n")
-            wealth = transact(wealth, new_money, exchange, type='EARN')
+            wealth = transact(wealth, new_money, exchange, transaction_type='EARN')
         if choice == 3:
             sys.exit()
